354f63fa2dc1b0d764360d4863a984cd38a0e45a
Beatrice Gachigi <beatricegachigi@gmail.com>
2024-03-24 22:14:43 +0300

    modifying

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+  const employee = {
+    employeeName: "jane",
+    streetAddress : "Anita",
+};
+  const employData = updateEmployeeWithKeyAndValue(employee, "role", "secretary");
+  
+  employData;
+  
+  function deleteFromEmployeeByKey(obj, key, value) {
+    obj[key] = value; //Why are we using bracket notation here?
+  
+    return obj;
+  }
+  delete Employee.firstname;
+  
+  employData;
+
+
+  
+
+
+   
+
+
+
+    
